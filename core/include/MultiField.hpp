c874055550a2d14a8fb707e1fecf2aba022e7af2
Robert May <robert.may@protonmail.com>
2024-09-22 13:47:41 -0600

    Fix broken copy in CMake build script.

--- core/include/MultiField.hpp
+++ core/include/MultiField.hpp
@@ -14,8 +14,10 @@ namespace FlexKit
 
 		MultiField() = default;
 
+
 		MultiField(iAllocator& IN_allocator) : 
 			allocator{ IN_allocator } {}
+
 
 		MultiField(const MultiField& rhs) : 
 			allocator	{ rhs.allocator }
@@ -25,6 +27,7 @@ namespace FlexKit
 			for (auto&& values : rhs)
 				push_back(values);
 		}
+
 
 		MultiField(MultiField&& rhs) noexcept :
 			allocator	{ rhs.allocator },
@@ -37,6 +40,7 @@ namespace FlexKit
 			rhs.used		= 0;
 			rhs.allocator	= nullptr;
 		}
+
 
 		MultiField& operator = (const MultiField& rhs)
 		{
@@ -53,6 +57,7 @@ namespace FlexKit
 			return *this;
 		}
 
+
 		MultiField& operator = (MultiField&& rhs)
 		{
 			allocator	= rhs.allocator;
@@ -66,6 +71,7 @@ namespace FlexKit
 			rhs.allocator	= nullptr;
 			return *this;
 		}
+
 
 		~MultiField()
 		{
@@ -218,11 +224,13 @@ namespace FlexKit
 				});
 		}
 
+
 		auto front()
 		{
 			FK_ASSERT(used >= 1);
 			return (*this)[0];
 		}
+
 
 		auto back()
 		{
@@ -588,3 +596,28 @@ namespace FlexKit
 		FieldContainer			fields;
 	};
 }
+
+
+/**********************************************************************
+
+Copyright (c) 2024 Robert May
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+**********************************************************************/
